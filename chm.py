6588bdfbdee0a905a2a2c5d88860291dfb93770b
Florian Ludwig <f.ludwig@greyrook.com>
2018-04-14 17:17:10 +0200

    timeouts

--- chm.py
+++ chm.py
@@ -6,18 +6,22 @@ import requests
 
 
 def main():
+    frequency = 0.5
+    request_timeout = 0.5
     code = collections.Counter()
     while True:
         t = time.time()
         try:
-            response = requests.get(sys.argv[1])
+            response = requests.get(sys.argv[1], timeout=request_timeout)
             code[response.status_code] += 1
         except requests.exceptions.ConnectionError:
             code['ConnectionError'] += 1
+        except requests.exceptions.ReadTimeout:
+            code['timeout'] += 1
         print(code)
         diff = time.time() -t
-        if diff < 1:
-            time.sleep(1 - diff)
+        if diff < frequency:
+            time.sleep(frequency - diff)
 
 
 if __name__ == '__main__':
